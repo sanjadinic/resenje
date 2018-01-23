5f8ab032657d57fd37ccfd1a58436fc97fa6caec
sanjadjak <sanya.dinic@gmail.com>
2018-01-23 21:05:51 +0100

    adding javascript

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+for (var i = 0; i <= 100; i++) {
+    if (i % 3 = 0) {
+        alert("Fizz")
+    }
+    if (i % 5 = 0) {
+        alert("Buzz")
+    }
+    if (i % 3 = 0 && i % 5 = 0) {
+        alert("FizzBuzz")
+    } else {
+        return i
+    }
